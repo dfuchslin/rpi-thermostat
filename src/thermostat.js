7c2577c640355fab18711a31e20428c0fb25dbc6
David Füchslin <david.fuchslin@gmail.com>
2018-12-31 19:13:47 +0100

    adjust speed curve, make events work across classes

--- src/thermostat.js
+++ src/thermostat.js
@@ -4,8 +4,6 @@ const UPDATE_INTERVAL = 5000
 module.exports = class Thermostat {
   constructor() {
     this.eventEmitter = new events.EventEmitter()
-    this.eventEmitter.on('temp', this.handleTemperatureEvent)
-    this.eventEmitter.on('speed', this.handleFanSpeedEvent)
   }
   destroy() {
     this.sensor.destroy()
@@ -20,30 +18,31 @@ module.exports = class Thermostat {
     this.fan = fan
   }
   start() {
+    this.eventEmitter.on('temp', (status) => {
+      if (status.error) {
+        console.log('Temperature sensor error!', status.error)
+        return
+      }
+      console.log(`handleTemp temp:${JSON.stringify(status)}`)
+      let speed = 0
+      if (status.temperature > 30.0) {
+        speed = 100
+      } else if (status.temperature > 28) {
+        speed = 75
+      } else if (status.temperature > 25) {
+        speed = 50
+      } else if (status.temperature > 22) {
+        speed = 25
+      }
+      this.fan.setSpeed(speed)
+    })
+    this.eventEmitter.on('speed', (fanSpeed) => {
+      console.log(`handleSpeed speed:${fanSpeed}`)
+    })
     setInterval(() => {
       this.sensor.readTemperature()
       this.fan.readSpeed()
     }, UPDATE_INTERVAL)
   }
-  handleTemperatureEvent(status) {
-    if (status.error) {
-      console.log('Temperature sensor error!', status.error)
-      return 
-    }
-    console.log(`handleTemp temp:${JSON.stringify(status)}`)
-    if (status.temperature > 30.0) {
-      this.fan.setSpeed(100)
-    } else if (status.temperature > 27) {
-      this.fan.setSpeed(75)
-    } else if (status.temperature > 25) {
-      this.fan.setSpeed(50)
-    } else if (status.temperature > 22) {
-      this.fan.setSpeed(25)
-    } else {
-      this.fan.setSpeed(0)
-    }
-  }
-  handleFanSpeedEvent(fanSpeed) {
-    console.log(`handleSpeed speed:${fanSpeed}`)
-  }
+
 }
